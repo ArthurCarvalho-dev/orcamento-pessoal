978a5ebf6020f7637fd119835ec52ed40d6a876b
Arthur Carvalho <arthur_vitorio@outlook.com>
2025-08-26 00:22:25 -0300

    Revert "Src"

--- components/Botao.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Botao({ children, onClick, variant='primary', type='button' }) {
-  return (
-    <button className={variant} onClick={onClick} type={type}>
-      {children}
-    </button>
-  )
-}
